3fc1a0403a394bdf87095c3269454714c98f2166
Joshua-Talltree <joshua.scott.talltree@gmail.com>
2021-01-23 23:41:16 -0600

    here's the next iteration of the construction app

--- app/src/main/java/com/example/construction_sevices/MainActivity.java
+++ app/src/main/java/com/example/construction_sevices/MainActivity.java
@@ -3,14 +3,37 @@ package com.example.construction_sevices;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    EditText edit1;
+    ImageView img1;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
+    }
 
+    public boolean getText() {
 
+        edit1 = (EditText) findViewById(R.id.text);
+        if ((edit1 == null) || (edit1.equals(""))) {
+
+            String result = "Please enter a value";
+            assert edit1 != null;
+            edit1.setText(result);
+
+            return false;
+        } else {
+
+        }
+        return true;
     }
 }
+
